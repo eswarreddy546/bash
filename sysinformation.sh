6ebe5ab888e630339b3bd9198e59fa13a572d20d
Asri <asri>
2025-09-21 20:51:39 +0530

    initialize the  values

--- sysinformation.sh
+++ sysinformation.sh
@@ -4,14 +4,21 @@ echo "---------------------------"
 echo "System & User Information"
 echo "---------------------------"
 
-echo "Current user        : $USER"       # logged-in user
-echo "Home directory      : $HOME"       # user’s home path
-echo "Current shell       : $SHELL"      # shell being used
-echo "Current directory   : $PWD"        # present working directory
-echo "Process ID (PID)    : $$"          # process ID of this script
-echo "Parent Process ID   : $PPID"       # process ID of parent shell
-echo "Number of arguments : $#"          # total arguments given to script
-echo "All arguments       : $@"          # all arguments passed
-echo "Exit status (last)  : $?"          # exit status of last command
-echo "Hostname            : $HOSTNAME"   # server/instance name
-echo "Date & Time         : $(date)"     # current system date
+echo "Current user        : $USER"
+echo "Home directory      : $HOME"
+echo "Current shell       : $SHELL"
+echo "Current directory   : $PWD"
+echo "Process ID (PID)    : $$"
+echo "Parent Process ID   : $PPID"
+echo "Number of arguments : $#"
+echo "All arguments       : $@"
+echo "Exit status (last)  : $?"
+echo "Hostname            : $HOSTNAME"
+echo "Date & Time         : $(date)"
+
+# 🔑 New additions
+PRIVATE_IP=$(hostname -I | awk '{print $1}')
+PUBLIC_IP=$(curl -s ifconfig.me)
+
+echo "Private IP          : $PRIVATE_IP"
+echo "Public IP           : $PUBLIC_IP"
