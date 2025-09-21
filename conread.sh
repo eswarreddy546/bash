608de90301b8a7900c5cbaf23e598b1aa26526a8
Asri <asri>
2025-09-21 20:20:22 +0530

    initialize the  values

---
+++ conread.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read name
+echo : " $name : Hi Neeraj,How are you ?"
+echo : "$name : hello, Eswar I am fine, how about you ?"
+echo : "$name : Good ,Neeraja Thank you for asking "
+echo : "$name : Hello"
