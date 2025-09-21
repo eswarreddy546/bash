f1f1e6febd8fbcb8df5a8dd76f43f751aefa6a15
Asri <asri>
2025-09-21 19:57:28 +0530

    initialize the  values

---
+++ initialize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+P1=neeraj
+P2=Eswar
+
+echo : "$P2 : Hi Neeraj,How are you ?"
+echo : "$P1 : hello, Eswar I am fine, how about you ?"
+echo : "$P2 : Good ,Neeraja Thank you for asking "
+echo : "$P1 : Hello"
