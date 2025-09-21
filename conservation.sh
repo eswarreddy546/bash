8cd506e5993664a8b2faba7dd128478da16afee4
Asri <asri>
2025-09-21 19:51:11 +0530

    new code push

---
+++ conservation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo : "Eswar : Hi Neeraj,How are you ?"
+echo : "Neeraj : hello, Eswar I am fine, how about you ?"
+echo : "Eswar : Good ,Neeraja Thank you for asking "
+echo : "Neeraj : Hello"
